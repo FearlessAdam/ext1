0b1489785dc866e0abdd648c0d408058dc9c203e
FearlessAdam <144472440+FearlessAdam@users.noreply.github.com>
2023-11-30 13:23:01 +0530

    Hi

--- main.ts
+++ main.ts
@@ -2,30 +2,34 @@ namespace SpriteKind {
     export const LoadingScreen = SpriteKind.create()
     export const TopTxt = SpriteKind.create()
 }
-let TopText = sprites.create(assets.image`myImage0`, SpriteKind.TopTxt)
-TopText.setScale(3, ScaleAnchor.Middle)
-let LoadingBar = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.LoadingScreen)
-LoadingBar.setScale(9, ScaleAnchor.Middle)
-animation.runImageAnimation(
-LoadingBar,
-assets.animation`LoadScreenAnim`,
-200,
-true
-)
+let TopText: Sprite = null
+let LoadingBar: Sprite = null
+function LoadScr () {
+    TopText = sprites.create(assets.image`myImage0`, SpriteKind.TopTxt)
+    TopText.setScale(3, ScaleAnchor.Middle)
+    LoadingBar = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.LoadingScreen)
+    LoadingBar.setScale(9, ScaleAnchor.Middle)
+    animation.runImageAnimation(
+    LoadingBar,
+    assets.animation`LoadScreenAnim`,
+    200,
+    true
+    )
+}
